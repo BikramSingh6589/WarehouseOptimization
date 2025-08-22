eec11e9af31e1ae110ca0cc18f3f596fb958021a
BikoMaster6589 <bikomaster9459@gmail.com>
2025-08-22 17:28:06 +0530

    small change'

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const verifyOtp = require('./routes/VerificationOtpRoute.js');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(session);
